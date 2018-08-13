d5a72966026528b119b1f61f9372d84bad014ddf
fcosta3d <fcosta3d@gmail.com>
2018-08-13 18:00:15 -0300

    Arquivos iniciais do projeto

---
+++ spec/exemplo/exemploSpec.js
@@ -0,0 +1,9 @@
+describe('suite de testes de exemplo com nodejs',()=>{
+  it('deve demonstrar o uso do jasmine com o nodejs',()=>{
+    expect(true).toEqual(true);
+  })
+
+  it('deve demonstrar o uso do karma em modo escuta',()=>{
+    expect(1).toEqual(1);
+  })
+})
